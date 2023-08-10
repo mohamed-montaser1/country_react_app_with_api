c29fe307e78cb5270e133db9525503a75406b78d
mohamed montaser <mastrocoding@gmail.com>
2023-08-10 12:45:37 +0300

    change logo to be clickable

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,11 @@
 import useTheme from "@/context/themeContext";
+import Link from "next/link";
 
 function Logo() {
   const { theme } = useTheme();
   return (
     <h1 className={`text-lg font-semibold ${theme === "dark" ? "text-slate-200" : "text-[#111517]"} xs:text-2xl`}>
-      Where In The World?
+      <Link href={"/"}>Where In The World?</Link>
     </h1>
   );
 }
